2b890b4ddf6a1ecb88a288fcc2b2c8e9eb01d687
Kevin Song <kevinmnm2010@gmail.com>
2020-08-27 17:48:06 -0400

    made cities.js file

---
+++ src/components/cities-comp.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class='cities-section'>
+     <div class='cities-title'><u>Cities</u></div>
+     <br>
+     <div class='cities-loop'>
+        <div class='cities-name'></div>
+        <div class='cities-img-wrap'>
+           <img class='cities-img' :src="require(`@/assets/${}`)" alt="cities img" />
+        </div>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.cities-title{
+   font-size: 30px;
+   font-weight: bold;
+}
+
+.cities-section{
+   position: relative;
+   margin-top: 75px;
+   text-underline-position: under;
+   width: 75%;
+   margin: auto;
+}
+
+</style>
--- src/views/Places.vue
+++ src/views/Places.vue
@@ -1,16 +1,20 @@
 <template>
    <div class='island-page'>
       <island-comp></island-comp>
+
+      <cities-comp></cities-comp>
    </div>
 </template>
 
 <script>
 import island_comp from "@/components/island-comp.vue"
+import cities_comp from "@/components/cities-comp.vue"
 
 export default {
    name: "PlacesPageComp",
    components: {
-      "island-comp": island_comp
+      "island-comp": island_comp,
+      "cities-comp": cities_comp
    }
 };
 </script>
