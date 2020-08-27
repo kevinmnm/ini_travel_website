25ba03afd4833dc655775a07f51c1f9ac1adad7a
Kevin Song <kevinmnm2010@gmail.com>
2020-08-27 16:56:01 -0400

    figured how to loop img src

--- src/components/island-comp.vue
+++ src/components/island-comp.vue
@@ -2,17 +2,36 @@
   <div class='island-section'>
      <div>
         <div class='island-title'><u>Islands</u></div>
+        <br>
+        <div v-for="all in islands" :key="all.des">
+           <div class='island-name'>{{ all.island }}</div>
+           <div>
+              <img :src="require(`@/assets/${all.img}`)" />
+           </div>
+           <div>{{  }}</div>
+        </div>
      </div>
   </div>
 </template>
 
 <script>
+import islands_list from "@/islands.js"
+
 export default {
-   name: "islandComp"
+   name: "islandComp",
+   data(){
+      return{
+         islands: islands_list
+      }
+   }
 }
 </script>
 
 <style scoped>
+.island-name{
+   font-size: 25px;
+   font-weight: bold;
+}
 
 .island-title{
    font-size: 30px;
