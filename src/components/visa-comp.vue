62a7ab0b3198688c74fa6318dafbb48857193273
Kevin Song <kevinmnm2010@gmail.com>
2020-08-27 14:32:54 -0400

    add visa component

--- src/components/visa-comp.vue
+++ src/components/visa-comp.vue
@@ -1,24 +1,97 @@
 <template>
    <div class="visa-page">
+      <br>
       <div class='page-content'>
-         <div>Visa Info</div>
-         <div>*Applicable to US Citizens Only*</div>
+         <div class='visa-info'><u>Visa Info</u></div>
+         <br>
+         <code>*Applicable to US Citizens Only*</code>
+         <br><br>
          <div class='visa-flex'>
-            <div class='visa-type'></div>
-            <div class='visa-des'></div>
+            <div class='visa-type' v-for='(all, ind) in visa_info' :key='all + ind'>
+               <div class='visa-title' @click='visa_shower(ind)'>{{ all.visa_type }}</div>
+               <div class='visa-des' v-show='all.visa_show'>
+                  <hr>
+                  <div><span><u>Description:</u> </span> {{ ' '+all.visa_what }}</div>
+                  <div><span><u>Purpose:</u> </span>{{ ' '+all.visa_who }}</div>
+                  <div><span><u>Extension:</u></span>{{ ' '+all.visa_extend }}</div>
+                  <div><span><u>Requirement:</u></span>{{ ' '+all.visa_requirement }}</div>
+                  <div><span><u>Cost:</u></span>{{ ' '+all.visa_cost }}</div>
+               </div>
+            </div>
          </div>
       </div>
+      <br>
    </div>
 </template>
 
 <script>
-export default {};
+import visa_infos from "@/visa.js"
+
+export default {
+   name: "visaComp",
+   data(){
+      return{
+         visa_info: visa_infos
+      }
+   },
+   methods: {
+      visa_shower(ind){
+         this.visa_info[ind].visa_show = true;
+      }
+   }
+};
 </script>
 
 <style scoped>
+.visa-des{
+   font-size: 18px;
+   text-align: left;
+}
+
+.visa-des div{
+   margin-bottom: 7px;
+   color: rgb(66, 66, 66);
+}
+
+.visa-des span{
+   color: black;
+}
+
+.visa-type{
+   font-size: 22px;
+   margin-bottom: 10px;
+   padding: 5px;
+   box-sizing: border-box;
+   background: whiteSmoke;
+   box-shadow: 0 0 2px black;
+   border-radius: 3px;
+   cursor: pointer;
+}
+
+.visa-type:hover{
+   background: white;
+}
+
+code{
+   color: red;
+   font-weight: bold;
+}
+
+.visa-info{
+   font-size: 30px;
+   font-weight: bold;
+}
+.page-content{
+   position: relative;
+   width: 80%;
+   margin: auto;
+}
+
 .visa-page {
    position: relative;
-   background: rgb(222, 184, 135);
+   /* background: rgb(222, 184, 135); */
+   background: rgb(255,222,173);
    margin-top: 65px;
+   text-underline-position: under;
 }
 </style>
--- src/visa.js
+++ src/visa.js
@@ -5,7 +5,8 @@ const visa_obj = [
       visa_who: 'For those who are coming for holiday or for any short term event.',
       visa_extend: 'No.',
       visa_requirement: 'There are no requirements, you receive a free stamp in your passport at the airport upon your arrival. It is eligible for citizens of these 169 countries.',
-      visa_cost: 'Free'
+      visa_cost: 'Free',
+      visa_show: false
    },
    {
       visa_type: 'Visa on Arrival (VOA) with extension',
@@ -13,7 +14,8 @@ const visa_obj = [
       visa_who: 'For those who come for holiday or for participating at any social, cultural events. Working and doing business activities are not allowed with this visa.',
       visa_extend: 'Yes, one time for an additional 30 days',
       visa_requirement: 'Passport which must be valid 6 months from entry date, return flight ticket.',
-      visa_cost: '35 USD (payable at the Visa counter at the airport), extension is 35 USD'
+      visa_cost: '35 USD (payable at the Visa counter at the airport), extension is 35 USD',
+      visa_show: false
    },
    {
       visa_type: 'Social / Cultural visa',
@@ -21,7 +23,8 @@ const visa_obj = [
       visa_who: 'For those who are coming with studying purpose, through foreign exchange programs or visiting family and relatives. It is not allowed to work or take up employment with this visa.',
       visa_extend: 'Yes, up to 180 days (almost 6 months) and should be extended every 30 days.',
       visa_requirement: 'Passport and photos, Application form from Embassy, Sponsor letter from school or an Indonesian citizen, KTP from your sponsor, return ticket. (May vary between different embassies & if you use an agent for applying)',
-      visa_cost: '~50 USD, extensions around 30 USD/ 30 days'
+      visa_cost: '~50 USD, extensions around 30 USD/ 30 days',
+      visa_show: false
    },
    {
       visa_type: 'Business visa',
@@ -29,7 +32,8 @@ const visa_obj = [
       visa_who: 'For those who are coming with business purpose – meeting business partners, participate on conferences or meetings & for researching business opportunities in Indonesia. It is not allowed to take up employment with this visa.',
       visa_extend: 'Yes, the single entry 60 days visa can be extended up to 180 days, extendable every 30 days. For the multiple entry 12 months visa you are required to exit the country every 60 days and come back again.',
       visa_requirement: 'Passport valid for minimum 6 months, photo, CV, Sponsor letter or official invitation and documents from an Indonesian company. For the 12 months visa you need also a special permit (TELEX).',
-      visa_cost: 'Single entry visa costs ~50 USD and Multiple entry visa costs ~110 USD for 60 days'
+      visa_cost: 'Single entry visa costs ~50 USD and Multiple entry visa costs ~110 USD for 60 days',
+      visa_show: false
    },
    {
       visa_type: 'Temporary Stay Permit – Working visa (KITAS)',
@@ -37,7 +41,8 @@ const visa_obj = [
       visa_who: 'For those who want to work in Bali and officially employed by an Indonesian based company.',
       visa_extend: 'Yes, the 6 months validity can be extended for another 6 months. The 5 year KITAS should be extended in every one year. You are required to leave the country before the visa expires and apply again.',
       visa_requirement: 'Sponsorship from an Indonesian based company proven with official documents, passport, CV, photos. (You are not able to apply for this kind of visa without sponsorship from a company based in Indonesia. Some visa agents may promise you otherwise, but be aware as those practices are illegal.)',
-      visa_cost: '~500 USD/year'
+      visa_cost: '~500 USD/year',
+      visa_show: false
    },
    {
       visa_type: 'Student visa (Student KITAS)',
@@ -45,7 +50,8 @@ const visa_obj = [
       visa_who: 'For students or scholarship participants.',
       visa_extend: 'Yes, up to 5 years in every one year.',
       visa_requirement: 'Passport valid for minimum 6 months, photos, CV, Application form, Medical report, Sponsor letter from school',
-      visa_cost: '~370 USD/year'
+      visa_cost: '~370 USD/year',
+      visa_show: false
    },
    {
       visa_type: 'Retirement visa (Retirement KITAS)',
@@ -53,7 +59,8 @@ const visa_obj = [
       visa_who: 'If you are 55 years old or older and would like to spend your years of retirement in Bali. Working, earning money or business activities are not allowed with this visa.',
       visa_extend: 'Yes, every one year.',
       visa_requirement: 'Passport valid for minimum 18 months, Health and life insurance, proof about receiving pension, CV, statement that you employ an Indonesian citizen for informal assistance.',
-      visa_cost: '~600 USD/year'
+      visa_cost: '~600 USD/year',
+      visa_show: false
    },
    {
       visa_type: 'Family visa (Family KITAS)',
@@ -61,6 +68,9 @@ const visa_obj = [
       visa_who: 'For spouse, family members or children whose parents/wife/husband is an Indonesian citizen or a KITAS holder.',
       visa_extend: 'Yes, up to 5 years, every one year.',
       visa_requirement: 'Passport valid for minimum 18 months, Marriage certificate or Birth certificate – other documents depend on whether the sponsor is an Indonesian citizen or foreign family member with KITAS.',
-      visa_cost: '~340 USD/year'
+      visa_cost: '~340 USD/year',
+      visa_show: false
    }
 ];
+
+export default visa_obj;
