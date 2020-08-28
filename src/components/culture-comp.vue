19890026c5d3a8a61fdf1e1c2135b32809134d84
Kevin Song <kevinmnm2010@gmail.com>
2020-08-27 22:31:24 -0400

    asset collected for foods section

--- src/components/culture-comp.vue
+++ src/components/culture-comp.vue
@@ -3,18 +3,22 @@
       <culture-religion-comp></culture-religion-comp>
 
       <culture-etiquette-comp></culture-etiquette-comp>
+
+      <culture-foods-comp></culture-foods-comp>
    </div>
 </template>
 
 <script>
 import culture_religion_comp from "@/components/culture-religion-comp.vue"
 import culture_etiquette_comp from "@/components/culture-etiquette-comp.vue"
+import culture_foods_comp from "@/components/culture-foods-comp.vue"
 
 export default {
    name: "cultureComp",
    components: {
       "culture-religion-comp": culture_religion_comp,
-      "culture-etiquette-comp": culture_etiquette_comp
+      "culture-etiquette-comp": culture_etiquette_comp,
+      "culture-foods-comp": culture_foods_comp
    },
 };
 </script>
---
+++ src/components/culture-foods-comp.vue
@@ -0,0 +1,40 @@
+<template>
+   <div class='culture-foods-section'>
+      <div class='foods-wrap'>
+         <div class='foods-title'><u>Foods</u></div>
+         <br>
+         <div class='foods-content'>
+            
+         </div>
+      </div>
+      <br>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "foodComp"
+}
+</script>
+
+<style scoped>
+
+.foods-title{
+   font-size: 30px;
+   font-weight: bold;
+   user-select: none;
+}
+
+.foods-wrap{
+   position: relative;
+   margin: auto;
+   width: 75%;
+}
+
+.culture-foods-section{
+   position: relative;
+   width: 100%;
+   text-underline-position: under;
+}
+
+</style>
